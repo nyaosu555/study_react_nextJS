bb7e4e3fcb8c3e9288dbe6a3a140bf3abfae3b7b
nyaosu555 <oneichan555@gmail.com>
2023-04-24 12:10:11 +0900

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            et started by editing&nbsp;
             <code className={styles.code}>src/pages/index.js</code>
           </p>
           <div>
