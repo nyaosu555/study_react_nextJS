c899410a6269711ab684b52cc276ab77d59f3fc4
nyaosu555 <oneichan555@gmail.com>
2023-04-25 16:27:06 +0900

    #6LinkLinkコンポーネントを使う

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+import styles from './Header.module.css'
+import Link from 'next/link'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <Link href="/" legacyBehavior>
+        <a className={styles.anchor}>Index</a>
+      </Link>
+      <Link href="/about" legacyBehavior>
+        <a className={styles.anchor}>About</a>
+      </Link>
+    </header>
+  )
+}
